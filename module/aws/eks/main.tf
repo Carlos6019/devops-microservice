730c78fa1aa0104d99dd0be617b75fa218645c8a
Carlos Arias <carlos6019a@gmail.com>
2025-07-13 19:06:01 -0500

    fix module eks

--- module/aws/eks/main.tf
+++ module/aws/eks/main.tf
@@ -23,9 +23,33 @@ data "aws_subnets" "default" {
   }
 }
 
+# EKS Cluster IAM Role (required)
+resource "aws_iam_role" "eks_cluster" {
+  name = "${var.cluster_name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
 # EKS Cluster - AWS creates IAM roles automatically
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
   version  = var.kubernetes_version
 
   vpc_config {
@@ -33,6 +57,10 @@ resource "aws_eks_cluster" "main" {
     endpoint_private_access = false
     endpoint_public_access  = true
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
 
   tags = {
     Name = var.cluster_name
