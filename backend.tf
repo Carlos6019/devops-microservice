b36ffefbf38f79a0177764885ff777bb447ea0fe
Carlos Arias <carlos6019a@gmail.com>
2025-07-13 19:46:30 -0500

    fix bucker for terraform.state

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "devops-eks-terraform-state"
+    bucket  = "mydevopstf"
     key     = "eks-cluster/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
